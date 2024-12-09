1adf00a930b52a3958500abdcea127d4334c99ee
Pavlent1 <162441884+Pavlent1@users.noreply.github.com>
2024-12-09 13:35:13 +0100

    Zombie, bug fixxes

--- Assets/Scripts/Zombie/Zombie Controller.cs
+++ Assets/Scripts/Zombie/Zombie Controller.cs
@@ -13,49 +13,56 @@ using UnityEditor;
 [RequireComponent(typeof(NavMeshAgent))]
 public class ZombieController : MonoBehaviour
 {
-    private enum Behavior
+    public enum Behavior
     {
         None,
         Waiting_onPoint,
         Patrolling,
         Chasing,
         Attaking,
-        BroadCasting
+        BroadCasting,
+        Detecting_Player
     }
-    private Behavior behavior = Behavior.None;
+    public Behavior behavior = Behavior.None;
 
 
     [Header("General")]
-    [SerializeField, Tooltip("Will notify zombies in specific area that player was detected")] bool Should_Broadcast = false;
-    [SerializeField, Tooltip("Radius of broadcast area")] float Broadcast_radius = 5.0f;
     [SerializeField] LayerMask Zombie_Layer;
     [SerializeField] string Player_tag = "Player";
     private GameObject player;
     private NavMeshAgent agent;
+
+    [Header("Broadcast")]
+    [SerializeField, Tooltip("Will notify zombies in specific area that player was detected")] bool Should_Broadcast = false;
+    [SerializeField, Tooltip("Radius of broadcast area")] float Broadcast_radius = 5.0f;
     private bool Ready_to_broadcast = true;
     //Temperal
     private float broadcast_time = 2;
     private float broadcast_timer = 0;
     //Temperal
 
-
-    [Header("Attack and Detection")]
+    [Header("Detection")]
     [SerializeField, Tooltip("Distance on which zombie will see the player")] float Detection_Radius = 5.0f;
     [SerializeField, Range(1, 360), Tooltip("Angle in which zombie will see the player")] float Detection_Angle = 90f;
+    [SerializeField, Tooltip("Will determain whether zombie will notice player instantly or after some time")] bool Should_detection_time = true;
     [SerializeField, Tooltip("Time for which player will have to stay in sight of zombie in order to be detected")] float Detection_time = 3.0f;
     private float Detection_timer = 0;
 
-    [SerializeField] float Delay_between_attacks = 1.0f;
+    [Header("Chasing")]
     [SerializeField, Tooltip("Time for which zombie will remeber player even if player exits the range of detection")] float Remember_time = 10.0f;
-    [SerializeField, Tooltip("Rotation speed towards target when chasing")] float Attack_RS = 6f;
-    private float Remember_timer = 0;
+    [SerializeField, Tooltip("Rotation speed towards target when chasing")] float Chase_rotation_speed = 6f;
+    public float Remember_timer = 0;
+
+    [Header("Attack")]
+    [SerializeField] float Attack_Distance = 2f;
+    [SerializeField] float Delay_between_attacks = 1.0f;
     private bool Ready_to_attack = false;
 
     [Header("Patrolling")]
     [SerializeField] bool Should_Patrol = true;
     [SerializeField, Tooltip("Defines the patrol area")] Collider Patrol_Area;
-    [SerializeField, Tooltip("Max distance on which zombie can walk per one destination switch")] float MaxRadius = 10f;
-    [SerializeField, Tooltip("Rotation speed towards target when patrolling")] float Patrol_RS = 3f;
+    [SerializeField, Tooltip("Max distance on what patrolling point can be set")] float Max_Point_distance = 10f;
+    [SerializeField, Tooltip("Rotation speed towards target when patrolling")] float Patrol_rotation_speed = 3f;
     [SerializeField] float onPoint_time = 2f;
     [SerializeField, Tooltip("Maximum attempts to find a valid patrol point")] int MaxAttempts = 50;
     private Vector3 Patrol_point = Vector3.zero;
@@ -69,46 +76,58 @@ public class ZombieController : MonoBehaviour
         player = GameObject.FindWithTag(Player_tag);
         if (player == null) Debug.LogError("Could not find the player");
         Routine();
+        ResetDetectionTimer();
     }
 
     private void Update()
     {
-
+        
         if (IfSeeThePlayer())
         {
-            if (Should_Broadcast && Ready_to_broadcast) 
+            if (Should_detection_time && Detection_timer > 0 && Remember_timer <= 0)
             {
-                behavior = Behavior.BroadCasting;
-                broadcast_timer = broadcast_time;
-                FullStop();
+                behavior = Behavior.Detecting_Player;
             }
-            else if (behavior < Behavior.Chasing)
+            else
             {
-                FullStop();
-                behavior = Behavior.Chasing;
+                if (Should_Broadcast && Ready_to_broadcast)
+                {
+                    //Ready_to_broadcast = false;
+                    behavior = Behavior.BroadCasting;
+                    broadcast_timer = broadcast_time;
+                    FullStop();
+                }
+                else if (behavior < Behavior.Chasing || CheckOnTemperalBehaviors())
+                {
+                    FullStop();
+                    behavior = Behavior.Chasing;
+                }
+
+                SetRemeberTimer();
             }
 
-            SetRemeberTimer();
-
-        }
-
-        if (Remember_timer > 0)
-        {
-            Remember_timer -= Time.deltaTime;
         }
         else
         {
-            Ready_to_broadcast = true;
-            if (behavior > Behavior.Patrolling)
+            ResetDetectionTimer();
+
+            if (Remember_timer > 0)
             {
-                Routine();
+                Remember_timer -= Time.deltaTime;
+            }
+            else if (behavior != Behavior.BroadCasting)
+            {
+                Ready_to_broadcast = true;
+                ComeBackToRoutine();
             }
         }
+
+        
 
         switch (behavior)
         {
             case Behavior.Waiting_onPoint:
-
+                
                 if (onPoint_timer <= 0)
                 {
                     behavior = Behavior.Patrolling;
@@ -130,7 +149,7 @@ public class ZombieController : MonoBehaviour
                 }
                 if (Agent_dest_SET)
                 {
-                    RotateTo(Patrol_point, Patrol_RS);
+                    RotateTo(Patrol_point, Patrol_rotation_speed);
 
                     if(ReachedDestination(Patrol_point, 0.2f))
                     {
@@ -164,8 +183,16 @@ public class ZombieController : MonoBehaviour
 
             case Behavior.Chasing:
 
-                MoveTo(player.transform.position);
-                RotateTo(player.transform.position, Attack_RS);
+                if (!ReachedDestination(player.transform.position, Attack_Distance / 2))
+                {
+                    MoveTo(player.transform.position);
+                }
+                else
+                {
+                    FullStop();
+                }
+                
+                RotateTo(player.transform.position, Chase_rotation_speed);
 
                 break;
 
@@ -175,6 +202,40 @@ public class ZombieController : MonoBehaviour
 
 
                 break;
+
+            case Behavior.Detecting_Player:
+
+                FullStop();
+                RotateTo(player.transform.position, Patrol_rotation_speed / 2);
+                Detection_timer -= Time.deltaTime;
+
+                break;
+        }
+    }
+
+    private bool CheckOnTemperalBehaviors()
+    {
+        if (behavior >= Behavior.BroadCasting)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void ComeBackToRoutine()
+    {
+        if (behavior > Behavior.Patrolling)
+        {
+            FullStop();
+            Routine();
+        }
+    }
+
+    private void ResetDetectionTimer()
+    {
+        if (Should_detection_time)
+        {
+            Detection_timer = Detection_time;
         }
     }
 
@@ -217,15 +278,13 @@ public class ZombieController : MonoBehaviour
         
         if (angleToPlayer <= Detection_Angle / 2)
         {
+            
+            Debug.DrawRay(transform.position, directionToPlayer * Detection_Radius, Color.red);
             if (Physics.Raycast(transform.position, directionToPlayer, out RaycastHit hit, Detection_Radius))
             {
                 if (hit.transform.gameObject == player)
                 {
                     return true;
-                }
-                else
-                {
-                    Debug.Log("Player either behind the wall or its not working");
                 }
             }
         }
@@ -238,7 +297,7 @@ public class ZombieController : MonoBehaviour
 
         for (int i = 0; i < MaxAttempts; i++)
         {
-            Vector3 randomOffset = UnityEngine.Random.insideUnitSphere * MaxRadius;
+            Vector3 randomOffset = UnityEngine.Random.insideUnitSphere * Max_Point_distance;
             randomOffset.y = 0;
             Vector3 randomPoint = transform.position + randomOffset;
             randomPoint.y = patrolHeight;
@@ -269,8 +328,11 @@ public class ZombieController : MonoBehaviour
 
     private void FullStop()
     {
-        agent.SetDestination(transform.position);
-        Agent_dest_SET = false;
+        if (agent.remainingDistance > 0.1f)
+        {
+            agent.SetDestination(transform.position);
+            Agent_dest_SET = false;
+        }
     }
 
     private void RotateTo(Vector3 destination, float rotationSpeed)
