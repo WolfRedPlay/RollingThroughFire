56b10247554d98223840341642ca9737315c006e
lauortega1998 <ortegalautaro@gmail.com>
2024-12-10 15:47:52 +0100

    PhoneV2

---
+++ Assets/PhoneLogicUI.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using Unity.UI;
+
+public class PhoneLogicUI : MonoBehaviour
+{
+    public GameObject homeScreen;
+    public GameObject messagesScreen;
+    public GameObject settingsScreen;
+    public GameObject saveloadScreen;
+    public GameObject emergencyScreen;
+    private void CloseAllScreens()
+    {
+        //homeScreen.SetActive(false);
+        settingsScreen.SetActive(false);
+        messagesScreen.SetActive(false);
+        saveloadScreen.SetActive(false);
+    }
+
+    // Method to go to the Home screen
+    public void GoToHome()
+    {
+        homeScreen.SetActive(true);
+        CloseAllScreens();
+    }
+
+    // Method to open the Settings screen
+    public void OpenSettings()
+    {
+        homeScreen.SetActive(false);
+        settingsScreen.SetActive(true);
+    }
+
+    // Method to activate the Flashlight screen
+    public void ActivateFlashlight()
+    {
+        CloseAllScreens();
+        //flashlightScreen.SetActive(true);
+    }
+
+    // Method to open the Messages screen
+    public void OpenMessages()
+    {
+        homeScreen.SetActive(false);
+        messagesScreen.SetActive(true);
+    }
+
+    // Method to open the Save/Load screen
+    public void OpenSaveLoad()
+    {
+        homeScreen.SetActive(false);
+        saveloadScreen.SetActive(true);
+    }
+}
+
