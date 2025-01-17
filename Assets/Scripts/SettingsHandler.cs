78d71f9842b113f1ce8b593b4d82c864f9af57ff
WolfRedPlay <126975145+WolfRedPlay@users.noreply.github.com>
2025-01-17 23:09:36 +0100

    Toggle implmentation

--- Assets/Scripts/SettingsHandler.cs
+++ Assets/Scripts/SettingsHandler.cs
@@ -1,23 +1,22 @@
-using Unity.AppUI.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsHandler : MonoBehaviour, IDataPersistence
 {
     [SerializeField] bool _movementHelper = false;
+    [SerializeField] Toggle _movementHelperToggle;
 
-    Toggle _toggle;
 
     public bool MovementHelper {  get { return _movementHelper; } set { _movementHelper = value; } }
 
     public void LoadData(GameData data)
     {
         _movementHelper = data.MovementHelper;
+        _movementHelperToggle.isOn = _movementHelper;
     }
 
     public void SaveData(ref GameData data)
     {
-        
         data.MovementHelper = _movementHelper;
-
     }
 }
