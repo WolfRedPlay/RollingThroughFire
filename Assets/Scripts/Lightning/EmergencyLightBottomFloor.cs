ff700f237d095df9bbe8274726224eba9394aaca
lauortega1998 <ortegalautaro@gmail.com>
2025-01-19 23:43:32 +0100

    lights bottom floor

---
+++ Assets/Scripts/Lightning/EmergencyLightBottomFloor.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class EmergencyLightBottomFloor : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        EmergencyLightController(5000);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
