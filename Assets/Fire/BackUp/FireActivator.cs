9ca70ee7f82cff01abbc0b404bb0fc178e58dd06
Pavlent1 <162441884+Pavlent1@users.noreply.github.com>
2025-01-20 23:11:04 +0100

    FireBackUp

---
+++ Assets/Fire/BackUp/FireActivator.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class FireActivator : MonoBehaviour
+{
+    private SphereCollider m_Collider;
+    [SerializeField] private string FireTag = "Fire";
+    [SerializeField] private float SpeedOfGrowing = 1f;
+    [SerializeField] private float MaxRadius = 10f;
+
+    private void Awake()
+    {
+        m_Collider = GetComponent<SphereCollider>();
+        m_Collider.isTrigger = true;
+    }
+
+    private void Update()
+    {
+        if (m_Collider.radius < MaxRadius)
+        {
+            m_Collider.radius += SpeedOfGrowing * Time.deltaTime;
+            m_Collider.radius = Mathf.Min(m_Collider.radius, MaxRadius);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trtigger");
+        if (other.transform.CompareTag(FireTag))
+        {
+            Debug.Log("Found fire");
+            foreach (Transform child in other.transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+    }
+}
